c9accbf49265ddb6128326cc0b11780cbd97fadf
Muqsit Rayyan <notmuqsit@gmail.com>
2026-09-02 18:43:55 +0800

    lambda: cleanup impl

--- lambda-function/src/main.rs
+++ lambda-function/src/main.rs
@@ -1,20 +1,20 @@
-use std::{net::{ToSocketAddrs, UdpSocket}, time::Duration};
-use lambda_http::{http::Method, run, service_fn, Body, Error, Request, Response};
+use std::{net::ToSocketAddrs, time::Duration};
+use lambda_http::{run, service_fn, Body, Error, Request, Response};
+use tokio::{net::UdpSocket, time::timeout};
 
-const P: &[u8] = b"\x01\0\0\0\0\0\0\0\0\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78\0\0\0\0\0\0\0\0";
+const PING_PACKET: &[u8] = b"\x01\0\0\0\0\0\0\0\0\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78\0\0\0\0\0\0\0\0";
 
-async fn f(r: Request) -> Result<Response<Body>, Error> {
-    let x = r.uri().path().trim_start_matches('/').trim();
-    let a = x.to_socket_addrs()?.next().ok_or("no address")?;
-    let s = UdpSocket::bind(if a.is_ipv6() { "[::]:0" } else { "0.0.0.0:0" })?;
-    s.set_read_timeout(Some(Duration::from_secs(3)))?;
-    s.send_to(P, a)?;
-    let mut b = [0; 2048];
-    let (n, _) = s.recv_from(&mut b)?;
-    Ok(Response::new(Body::Binary(b[..n].to_vec())))
+async fn handler(request: Request) -> Result<Response<Body>, Error> {
+    let target = request.uri().path().trim_start_matches('/').trim();
+    let address = target.to_socket_addrs()?.next().ok_or("no address")?;
+    let socket = UdpSocket::bind(if address.is_ipv6() { "[::]:0" } else { "0.0.0.0:0" }).await?;
+    socket.send_to(PING_PACKET, address).await?;
+    let mut buffer = [0; 2048];
+    let (len, _) = timeout(Duration::from_secs(3), socket.recv_from(&mut buffer)).await??;
+    Ok(Response::new(Body::Binary(buffer[..len].to_vec())))
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Error> {
-    run(service_fn(f)).await
+    run(service_fn(handler)).await
 }
